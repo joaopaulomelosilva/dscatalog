0ec0e7c836dab19be0d3cc87a2012733dab8cd8c
João Paulo de Melo Silva <33991065+joaopaulomelosilva@users.noreply.github.com>
2022-12-22 17:29:40 -0300

    ProductDetails Layout Mobile

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,35 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css';
 
 const ProductDetails = () => {
 
     return (
-        <h1>Product Details</h1>
-
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon></ArrowIcon>
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt=" Nome do Produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do Produto</h1>
+                            <ProductPrice price={2222.67}></ProductPrice>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, consequatur.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 
 }
